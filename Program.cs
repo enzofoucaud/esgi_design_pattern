6ed1fe82585c42382646021d974e632b88535c7e
Enzo Foucaud <foucauden@gmail.com>
2022-07-04 10:20:41 +0200

    New: Class Automobile

--- Program.cs
+++ Program.cs
@@ -10,3 +10,26 @@ namespace esgi_design_pattern
         }
     }
 }
+
+#############################################################################################
+
+// Class Automobile
+public abstract class Automobile
+{
+    protected string modele;
+    protected string couleur;
+    protected int puissance;
+    protected double espace;
+
+    public Automobile(string modele, string couleur, int puissance, double espace)
+    {
+        this.modele = modele;
+        this.couleur = couleur;
+        this.puissance = puissance;
+        this.espace = espace;
+    }
+
+    public abstract void afficherCaracteristiques();
+}
+
+#############################################################################################

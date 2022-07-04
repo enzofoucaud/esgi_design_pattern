e2466cbe9e51211ee9db65bb2b1bbe4c36a4ef19
Enzo Foucaud <foucauden@gmail.com>
2022-07-04 10:23:50 +0200

    New: Class AutomobileElectrique : Automobile

--- Program.cs
+++ Program.cs
@@ -33,3 +33,22 @@ public abstract class Automobile
 }
 
 #############################################################################################
+
+// Class AutomobileElectrique : Automobile
+public class AutomobileElectrique : Automobile
+{
+    public AutomobileElectrique(string modele, string couleur, int puissance, double espace) : base(modele, couleur, puissance, espace)
+    {
+    }
+
+    public override void afficherCaracteristiques()
+    {
+        Console.WriteLine("Automobile électrique :");
+        Console.WriteLine("Modele : " + modele);
+        Console.WriteLine("Couleur : " + couleur);
+        Console.WriteLine("Puissance : " + puissance);
+        Console.WriteLine("Espace : " + espace);
+    }
+}
+
+#############################################################################################

77df8e98b438bb9cc59fb0ee4d43c3aac574efe8
Enzo Foucaud <foucauden@gmail.com>
2022-07-05 16:33:09 +0200

    WIP: cours

--- example.cs
+++ example.cs
@@ -1,5 +1,10 @@
-//  Le singleton = un objet unique qui peut être instancié une seule fois.
-//  L'abastact est susceptible d'utiliser ce type de singleton.
-//  Notre app utilisera la classe Liasse vierge (LiasseVierge) qui aura une seule instance.
-//  Chaque classe qui utilise la Liasse vierge doit avoir accès à la même instance.
-//  On doit surtout s'arranger pour qu'on ne puisse pas en créer de nouvelles.
+// Pattern de structure
+
+// Permet de faciliter l'indépendance de l'interface d'un objet et de son implémentation.
+// En fournissant les interfaces, ce pattern permet d'encapsuler la composition de l'objet.
+// Cela augmente le niveau d'abastraction d'un système donnée un peu à la manière des patterns de création qui encapsulent la création de l'objet.
+// Ces patternes mettent en avant les interfaces.
+
+// Ex : Composition vs Heritage
+// Composition créer une relation entre les classes. (a un)
+// Héritage est-une relation entre les classes. (est un)

19f002b923c731cc11dc9f8ad73e5873c977d7d2
Enzo Foucaud <foucauden@gmail.com>
2022-07-05 16:01:21 +0200

    Refactor: Prettier

--- SINGLETON_Vendeur.cs
+++ SINGLETON_Vendeur.cs
@@ -5,19 +5,20 @@ public class Vendeur
 {
   private static Vendeur _instance = null;
   private List<Document> documents = new List<Document>();
-  private Vendeur()
-  {
-  }
+  private Vendeur(){}
+  
   public static Vendeur Instance()
   {
     if (_instance == null)
       _instance = new Vendeur();
     return _instance;
   }
+
   public void ajoute(Document doc)
   {
     documents.Add(doc);
   }
+  
   public void imprime()
   {
     foreach (Document doc in documents)
